3ae8db667ff6760b8a046bb99489bae8641fcefe
hex248 <oliverbryan248@gmail.com>
2022-10-02 20:29:19 +0100

    About page done

--- src/About.js
+++ src/About.js
@@ -1,9 +1,29 @@
 import React from "react";
+import MediaQuery from "react-responsive";
+import LightDark from "./LightDark";
 
 const About = () => {
     return (
         <>
-            <div className="main colour-transition"></div>
+            <MediaQuery minWidth={701}>
+                <LightDark />
+            </MediaQuery>
+            <div className="main colour-transition" id="aboutMain">
+                <h1 id="aboutTitle">ABOUT</h1>
+                <h1 id="aboutDescription">
+                    Portrait and street photographer
+                    <br />
+                    17 years old
+                    <br />
+                    Based in London
+                    <br />
+                    See more at:
+                    <br />
+                    <a href="https://home.oliverbryan.com" target="_blank" rel="noreferrer" className="embeddedLink">
+                        home.oliverbryan.com
+                    </a>
+                </h1>
+            </div>
         </>
     );
 };
