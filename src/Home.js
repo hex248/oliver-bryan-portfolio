d7875b2ad1276d000c3889630a80edb09dfbb06e
hex248 <oliverbryan248@gmail.com>
2022-10-22 18:39:12 +0100

    Removed images

--- src/Home.js
+++ src/Home.js
@@ -9,12 +9,8 @@ const Home = () => {
                 <LightDark />
             </MediaQuery>
             <div className="main colour-transition" id="homeMain">
-                <MediaQuery minWidth={701}>
-                    <img id="homeBackground" src="/photos/me/Home.jpg" alt="home"></img>
-                </MediaQuery>
-                <MediaQuery maxWidth={700}>
-                    <img id="homeBackground" src="/photos/me/HomeMobile.jpg" alt="home"></img>
-                </MediaQuery>
+                <MediaQuery minWidth={701}>{/* <img id="homeBackground" src="/photos/me/Home.jpg" alt="home"></img> */}</MediaQuery>
+                <MediaQuery maxWidth={700}>{/* <img id="homeBackground" src="/photos/me/HomeMobile.jpg" alt="home"></img> */}</MediaQuery>
             </div>
         </>
     );
