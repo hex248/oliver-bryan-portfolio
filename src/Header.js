c047173c6c6a4017979e41172ccbd8337b92b9b7
Oliver Bryan <oliverbryan248@gmail.com>
2023-05-09 01:02:22 +0100

    New logo & events update

--- src/Header.js
+++ src/Header.js
@@ -4,36 +4,42 @@ import MediaQuery from "react-responsive";
 import LightDark from "./LightDark";
 
 const Header = () => {
-    return (
-        <>
-            <MediaQuery maxWidth={700}>
-                <LightDark />
-            </MediaQuery>
+  return (
+    <>
+      <MediaQuery maxWidth={700}>
+        <LightDark />
+      </MediaQuery>
 
-            <div className="header colour-transition">
-                <Link to="/" id="pageTitle" className="link">
-                    OLIVER BRYAN
-                </Link>
-                <div className="links">
-                    <Link to="/events" className="link">
-                        Events
-                    </Link>
-                    <Link to="/portraits" className="link">
-                        Portraits
-                    </Link>
-                    <Link to="/street" className="link">
-                        Street
-                    </Link>
-                    <Link to="/booking" className="link">
-                        Booking
-                    </Link>
-                    <Link to="/about" className="link">
-                        About
-                    </Link>
-                </div>
-            </div>
-        </>
-    );
+      <div className="header colour-transition">
+        <Link to="/" id="pageTitle" className="link">
+          <img
+            src="/made-by-oliver-bryan.svg"
+            alt="Made by Oliver Bryan"
+            style={{
+              filter: "invert(100%)",
+            }}
+          />
+        </Link>
+        <div className="links">
+          <Link to="/events" className="link">
+            Events
+          </Link>
+          <Link to="/portraits" className="link">
+            Portraits
+          </Link>
+          <Link to="/street" className="link">
+            Street
+          </Link>
+          <Link to="/booking" className="link">
+            Booking
+          </Link>
+          <Link to="/about" className="link">
+            About
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
