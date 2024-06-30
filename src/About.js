1da463b9057346a6dca2bf38298514be0962c4c0
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2024-06-30 13:39:16 +0100

    18->19

--- src/About.js
+++ src/About.js
@@ -3,39 +3,39 @@ import MediaQuery from "react-responsive";
 import LightDark from "./LightDark";
 
 const About = () => {
-  return (
-    <>
-      <MediaQuery minWidth={701}>
-        <LightDark />
-      </MediaQuery>
-      <div className="main colour-transition" id="aboutMain">
-        <h1 id="aboutTitle">About</h1>
-        <h1 id="aboutDescription">
-          18 years old
-          <br />
-          Based in London
-          <br />
-          <br />
-          <a
-            href="https://instagram.com/oliverbryann"
-            target="_blank"
-            rel="noreferrer"
-            className="embeddedLink"
-          >
-            Instagram
-          </a>
-        </h1>
-        <img
-          src="/made-by-oliver-bryan.svg"
-          alt="Made by Oliver Bryan"
-          style={{
-            filter: "invert(100%)",
-          }}
-          id="aboutFooter"
-        />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <MediaQuery minWidth={701}>
+                <LightDark />
+            </MediaQuery>
+            <div className="main colour-transition" id="aboutMain">
+                <h1 id="aboutTitle">About</h1>
+                <h1 id="aboutDescription">
+                    19 years old
+                    <br />
+                    Based in London
+                    <br />
+                    <br />
+                    <a
+                        href="https://instagram.com/oliverbryann"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="embeddedLink"
+                    >
+                        Instagram
+                    </a>
+                </h1>
+                <img
+                    src="/made-by-oliver-bryan.svg"
+                    alt="Made by Oliver Bryan"
+                    style={{
+                        filter: "invert(100%)",
+                    }}
+                    id="aboutFooter"
+                />
+            </div>
+        </>
+    );
 };
 
 export default About;
